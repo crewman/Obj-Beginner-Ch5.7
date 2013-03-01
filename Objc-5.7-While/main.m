e8b13c4b5807b16f3d0e2e82a28042bec23c6aa5
Leif Petersen <leifthelucky@gmail.com>
2013-02-28 21:39:03 -0800

    Initial Commit

---
+++ Objc-5.7-While/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Objc-5.7-While
+//
+//  Created by Leif Petersen on 13-02-28.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
